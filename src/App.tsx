9eaae97040aeb5729f3f0e29a88cc9fe539545b5
unknown <matsuda.corretora@gmail.com>
2026-01-28 13:55:03 -0300

    feat: navbar finalizada e integrada ao app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes } from "react-router-dom"
 import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar" // 1. Importe sua Navbar aqui
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          {/* Rotas aqui */}
-
-        </Routes>
-         <Footer />
+        <Navbar /> {/* 2. Coloque a Navbar aqui para ela fixar no topo */}
+        <div className='min-h-[80vh]'> {/* 3. Dica: div para empurrar o footer para baixo */}
+          <Routes>
+            {/* Rotas aqui */}
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </>
   )
