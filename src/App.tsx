69979e084717c387e78d29e056dde0717b83eae5
Daniel Ribeiro <dann.ribeiroo@gmail.com>
2026-01-28 11:35:22 -0300

    feat: cleanup and initial config

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes } from "react-router-dom"
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* Rotas aqui */}
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
